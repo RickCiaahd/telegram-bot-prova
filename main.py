0e91bfa9bb675ab867a137f65beb851296f4c920
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2025-06-16 10:38:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import telegram
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot attivo!"
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    if update.message and update.message.text:
+        chat_id = update.message.chat.id
+        text = update.message.text
+        bot.send_message(chat_id=chat_id, text=f"Hai detto: {text}")
+    return 'ok'
